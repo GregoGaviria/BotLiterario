d5134120429f9485aaf81ff290678cbed2432d67
Gregorio Gaviria <gregogaviria@gmail.com>
2026-03-01 18:12:30 -0600

    upload de files y prompt casi completo

---
+++ LoadAudio.py
@@ -0,0 +1,2 @@
+with open("transcript.txt", "w+") as f:
+    f.write("Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam quis sapien eu erat varius fermentum ut non metus. Pellentesque porttitor consectetur turpis, ac semper ligula tempus id. Donec a nibh rhoncus, tempor ex a, luctus lectus. Praesent hendrerit massa orci, eu luctus ante ornare eget. Duis fringilla sagittis suscipit. Aliquam.")
--- main.go
+++ main.go
@@ -1,11 +1,13 @@
 package main
 
 import (
+	"fmt"
 	"html/template"
 	"io"
 	"log"
 	"net/http"
 	"os"
+	"os/exec"
 	"time"
 )
 
@@ -21,6 +23,12 @@ func handleFile(w http.ResponseWriter, r *http.Request) {
 		w.WriteHeader(http.StatusMethodNotAllowed)
 		return
 	}
+	err := r.ParseMultipartForm(10 << 20)
+	if err != nil {
+		w.WriteHeader(http.StatusBadRequest)
+		w.Write([]byte(err.Error()))
+		return
+	}
 	file, fileHeader, err := r.FormFile("file")
 	if err != nil {
 		http.Error(w, err.Error(), http.StatusBadRequest)
@@ -28,24 +36,68 @@ func handleFile(w http.ResponseWriter, r *http.Request) {
 	}
 
 	defer file.Close()
-	dst, err := os.Create("./audioFiles/" + time.Now().String()+fileHeader.Filename)
+	dst, err := os.Create("./audioFiles/" + time.Now().String() + fileHeader.Filename)
 	if err != nil {
 		http.Error(w, err.Error(), http.StatusInternalServerError)
 		return
 	}
 	defer dst.Close()
 
-	_,err = io.Copy(dst,file)
-	if err != nil{
+	_, err = io.Copy(dst, file)
+	if err != nil {
 		http.Error(w, err.Error(), http.StatusInternalServerError)
 		return
 	}
+	cmd := exec.Command("python", "LoadAudio.py")
+	if err := cmd.Start(); err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	if err := cmd.Wait(); err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	w.Write([]byte("cargando audio..."))
+}
 
+func handlePrompt(w http.ResponseWriter, r *http.Request) {
+	if r.Method != "POST" {
+		w.WriteHeader(http.StatusMethodNotAllowed)
+		return
+	}
+	prompt := r.FormValue("prompt")
+	if prompt == "" {
+		http.Error(w, "no hay prompt", http.StatusBadRequest)
+		return
+	}
+	response, err := http.Get("localhost:8090?prompt=" + prompt)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	body, err := io.ReadAll(response.Body)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	response.Body.Close()
+	if response.StatusCode > 299 {
+		errstring := fmt.Sprintf(
+			"statuscode %d: body: %s",
+			response.StatusCode,
+			string(body),
+		)
+		http.Error(w, errstring, http.StatusInternalServerError)
+		return
+	}
+	w.Write(body)
 }
 
 func main() {
 	fs := http.FileServer(http.Dir("./static"))
 	http.Handle("/static/", http.StripPrefix("/static/", fs))
+	http.HandleFunc("/audio", handleFile)
+	http.HandleFunc("/prompt", handlePrompt)
 	http.HandleFunc("/", handleHome)
 	err := http.ListenAndServe(":8000", nil)
 	if err != nil {
