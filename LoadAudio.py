a857f85db62f9a0e5721e8180f48fdeb9c08bb17
Gregorio Gaviria <gregogaviria@gmail.com>
2026-04-10 21:22:26 -0600

    cambio de libreria de transcripcion a whisper

--- LoadAudio.py
+++ LoadAudio.py
@@ -1,23 +1,13 @@
-import speech_recognition as sr
-# from os import path
-from pydub import AudioSegment
 import argparse
-
+import whisper
 
 parser = argparse.ArgumentParser()
 parser.add_argument("-f", "--filename")
 args = parser.parse_args()
 filename = args.filename
 
-sound = AudioSegment.from_mp3(filename)
-sound.export("transcript.wav", format="wav")
-
-AUDIO_FILE = "transcript.wav"
-
-r = sr.Recognizer()
-with sr.AudioFile(AUDIO_FILE) as source:
-    audio = r.record(source)
-    output = r.recognize_google(audio)
+model = whisper.load_model("turbo")
+result = model.transcribe(filename)
 
 with open("transcript.txt", "w+") as f:
-    f.write(output)
+    f.write(result["text"])
